4eaba17c4e991fc2f4785996655f033606af47bd
JuanC <camiloflstudio@gmail.com>
2022-07-27 21:28:35 -0500

    Se crean consultas personalizadas para el log y para el task del repositorio

--- backend/src/main/java/org/sofka/mykrello/model/repository/LogRepository.java
+++ backend/src/main/java/org/sofka/mykrello/model/repository/LogRepository.java
@@ -2,5 +2,22 @@ package org.sofka.mykrello.model.repository;
 
 import org.sofka.mykrello.model.domain.LogDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface LogRepository extends JpaRepository<LogDomain, Integer>{ }
+import java.util.List;
+
+@Repository
+public interface LogRepository extends JpaRepository<LogDomain, Integer> {
+
+
+    @Query(value = "DELETE FROM LogDomain WHERE taskId= 28")
+    @Modifying
+    void deleteByTask(Integer taskId);
+    List<LogDomain> findLogDomainByTaskId(Integer taskId);
+
+
+}
+
+
--- backend/src/main/java/org/sofka/mykrello/model/repository/TaskRepository.java
+++ backend/src/main/java/org/sofka/mykrello/model/repository/TaskRepository.java
@@ -3,4 +3,11 @@ package org.sofka.mykrello.model.repository;
 import org.sofka.mykrello.model.domain.TaskDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<TaskDomain, Integer> { }
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<TaskDomain, Integer> {
+
+    List<TaskDomain> findAllByBoard(Integer idBoard);
+
+    List<TaskDomain> findAllByColumnAndAndBoard(Integer idColumn, Integer idBoard);
+}
