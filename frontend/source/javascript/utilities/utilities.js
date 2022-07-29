8b64dbd0ddee3a249de954c1edbbb0b02a5b95eb
camora1990 <moorales.camilo@gmail.com>
2022-07-29 10:30:31 -0500

    se crea clase para creacion de elementos en el DOM

---
+++ frontend/source/javascript/utilities/utilities.js
@@ -0,0 +1,33 @@
+/**
+ * Utilidades generales de la aplicacion como creacioin de componentes
+ * @class
+ * @author Camilo Morales Sanchez - Juan Camilo Castñeda Castro
+ */
+
+ class Utilities {
+    /**
+     *
+     * @param {String} tagName - etiqueta html
+     * @param {String []} classes - clases de la etiqueta
+     * @param {String} id -id de la etiqueta
+     * @param {String} name - nombre de la tiqueta
+     * @param {String} value - valor del campo
+     * @returns {HTMLElement} Etiqueta HTML
+     * @author Camilo Morales Sanchez
+     */
+    createComponent(tagName, classes = [], id = null, name = null, value = null) {
+      const tag = document.createElement(tagName);
+      classes.length > 0 && tag.classList.add(...classes);
+      id && (tag.id = id);
+      name && (tag.name = name);
+      value && tagName != "li" && (tag.value = value);
+      value && (tag.textContent = value);
+      value && (tag.innerText = value);
+      return tag;
+    }
+  }
+  
+  const utilities = new Utilities();
+  
+  export { utilities };
+  
