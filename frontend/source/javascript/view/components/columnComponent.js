121270843a0c039ff01a5d90529885f71df53baf
camora1990 <moorales.camilo@gmail.com>
2022-07-30 16:40:19 -0500

    se refactorizan los componentes de task y column

--- frontend/source/javascript/view/components/columnComponent.js
+++ frontend/source/javascript/view/components/columnComponent.js
@@ -1,27 +1,39 @@
+import { taskComponent } from "./taskComponent.js";
 
+export class ColumnComponent {
+  #component;
+  #parentNode;
+  #icons;
+  constructor(columns, row) {
+    this.#parentNode = row;
+    this.#icons = [
+      `<i class="fa-solid fa-hourglass-start"></i>`,
+      `<i class="fa-solid fa-spinner"></i>`,
+      `<i class="fa-solid fa-check"></i>`,
+    ];
+    this.#createColumn(columns);
+  }
 
-export class CoumnComponent {
-    #component
+  #createColumn(columns) {
 
-    constructor(columns) {
-        this.#component = this.#createColumn(columns);
-    }
-
-    #createColumn(columns) {
-        const button = `<button
+    columns.forEach((column, i) => {
+      const columFragmen = document.createElement("template");
+      columFragmen.innerHTML = `<div class="col col-lg-4 text-center">
+        <div class="container-task border-2 rounded">
+          <h3 class="colum">${column.name} ${this.#icons[i]}</h3>
+        </div>
+        <button data-columnId=${column.id}
         class="btn btn-outline-success form-control bg-dark custom-btn mt-2 mb-2 ">
         NuevaTarea <i class="fa-solid fa-circle-plus" id="btn-new-task"></i>
-        </button>`
+        </button>
+      </div>`;
+      const parentNodeTask =
+        columFragmen.content.querySelector(".container-task");
 
-        
-
-        const columnTemplate = `<div class="col col-lg-4 text-center">
-        <div class="container-task border-2 rounded">
-          <h3 class="colum">Por realizar <i class="fa-solid fa-hourglass-start"></i></i></h3>
-        </div>
-      </div>`
-    }
-
-    
-
+      column.tasks.forEach((task) => {
+        new taskComponent(task, parentNodeTask);
+      });
+      this.#parentNode.append(columFragmen.content);
+    });
+  }
 }
--- frontend/source/javascript/view/components/taskComponent.js
+++ frontend/source/javascript/view/components/taskComponent.js
@@ -1,27 +1,41 @@
 export class taskComponent {
+  #component;
+  #parenNode;
+  #task;
 
-    #component
+  constructor(task, parentNode) {
+    this.#parenNode = parentNode;
+    this.#task = task;
+    this.#createTask(task);
+  }
 
-    constructor(task){
-        this.component = this.#createTask(task)
-    }
-
-    #createTask(task){
-        const a = ` <div class="card card-task">
-            <div class="card-body text-light">
-            <p class="card-title"></p>
+  #createTask(task) {
+    const createdat = moment(task.createdAt).format("l");
+    const taskComponent = ` <div class="card card-task">
+            <div class="card-body text-light text-start">
+            <p class="card-title">${task.name}</p>
             <p class="card-text">
                 <i class="fa-solid fa-clock me-3"></i>
-                <span class="text-muted"></span>
+                <span class="text-muted">${createdat}</span>
             </p>
             <div class="d-flex justify-content-between">
                 <button
                 class="btn btn-outline-success bg-dark"
                 >Ver mas</button
                 >
+                <div>
+                <button id="delete-task" class="btn btn-danger">
+                  <i class="fa-solid fa-trash-can"></i>
+                </button>
+                <button   id="edit-task" class="btn btn-warning">
+                  <i class="fa-solid fa-file-pen"></i>
+                </button>
+              </div>
             </div>
             </div>
-            </div>`
-    }
-
+            </div>`;
+    const fragment = document.createElement("template");
+    fragment.innerHTML = taskComponent;
+    this.#parenNode.append(fragment.content);
+  }
 }
